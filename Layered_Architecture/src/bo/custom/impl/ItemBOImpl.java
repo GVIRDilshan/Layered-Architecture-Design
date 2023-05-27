f3b3be8522c975bcccaa0f00c38e7be0a5673bad
GVIRDilshan <ishanravidu750@gmail.com>
2023-05-27 22:40:55 +0530

    Edit ItemBOImpl Class

--- Layered_Architecture/src/bo/custom/impl/ItemBOImpl.java
+++ Layered_Architecture/src/bo/custom/impl/ItemBOImpl.java
@@ -1,6 +1,7 @@
 package bo.custom.impl;
 
 import bo.custom.ItemBO;
+import dao.DAOFactory;
 import dao.custom.ItemDAO;
 import dao.custom.impl.ItemDAOImpl;
 import model.ItemDTO;
@@ -9,39 +10,42 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemBOImpl implements ItemBO {
+
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
+
     @Override
     public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
+//        ItemDAO itemDAO = new ItemDAOImpl();
        return itemDAO.getAll();
     }
 
     @Override
     public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
+//        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.delete(code);
     }
 
     @Override
     public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
+//        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.add(dto);
     }
 
     @Override
     public boolean updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
+     //   ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.update(dto);
     }
 
     @Override
     public boolean existItem(String code) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
+       // ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.exist(code);
     }
 
     @Override
     public String generateNewCode() throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
+       // ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.generateNewID();
     }
 }
